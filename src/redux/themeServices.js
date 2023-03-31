ebb41ac9a4a69d10c997992b142c30cd07a17381
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-03-31 21:16:24 +0530

    front-end complete

---
+++ src/redux/themeServices.js
@@ -0,0 +1,10 @@
+export const themeAction = {
+    toogleTheme: (state) => {
+      if (state === "light") {
+        return "dark";
+      } else {
+        return "light";
+      }
+    },
+  };
+  
